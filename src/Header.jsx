18d3bc6e6330f3882ed531012fd19f6e8daf373a
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-08 13:28:42 +0530

    header functionality

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,2 +1,10 @@
 import React from "react";
+
+Headers = ()=> {
+    return (
+        <div className="herader">
+            <img src="./images/restaurant-logo.avif" alt="logo" className="logo"/>
+        </div>
+    )
+}
 export default Headers
