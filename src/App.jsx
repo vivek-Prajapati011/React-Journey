8bd753b49c274c3daf839e5434f4805047a5de3b
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-08 14:18:20 +0530

    import header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Header from './Header';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <Headers />
+      <Header /> 
+      <h2>Welcome to my app</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App; 
