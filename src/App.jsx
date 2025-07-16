d10a62317c2f132f11b8b68680ad8bd4c8385d77
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-16 20:09:35 +0530

    Adds Greeting component to the main application
    Integrates the Greeting component into the App, enhancing the user interface by providing a welcoming message.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Header from './Header';
 import './App.css';
 import CounterApp from '../useState/CounterApp';
+import Greeting from '../useState/greeting';
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <CounterApp />
+      <Greeting />
       <h2>Welcome to my app</h2>
     </>
   );
