2f6648add781d309258371b109903069d2545a49
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-16 18:36:23 +0530

    setting button logic

--- useState/CounterApp.jsx
+++ useState/CounterApp.jsx
@@ -2,7 +2,11 @@ import React from "react"
 import { useState } from "react"
 
 const CounterApp = () => {
-   
+    const [count, setcount] = useState(0)
+    const countterBtn = () => {
+        setcount(count+1)
+    }
+
 }
 
 export default CounterApp
