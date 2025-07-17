283e99182d045ac7eb2e10a5d10b44cd63e5698b
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-17 13:08:21 +0530

    Adds Logger component to App and adjusts commented sections
    Introduces the Logger component to enhance debugging capabilities.
    
    Modifies the structure of commented components for better clarity and organization.
    
    Prepares for future component integration by maintaining a clean codebase.

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import './App.css';
 import CounterApp from '../useState/CounterApp';
 import Greeting from '../useState/greeting';
 import Todo from '../useState/Todo';
+import Logger from '../useEffect/Logger';
 
 function App() {
   
 
   return (
     <>
-      <Todo />
-      {/* <CounterApp />
+      {/* <Todo />
+      <CounterApp />
       <Greeting />
       <h2>Welcome to my app</h2> */}
+      <Logger />
     </>
   );
 }
