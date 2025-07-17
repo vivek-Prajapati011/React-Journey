2a79d2f50651c78ea59e656ef55006e57f77c5f0
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-17 14:06:19 +0530

    Adds Timer component to the App
    Integrates the Timer component into the main application.
    
    This addition enhances the functionality of the app by providing a new feature for time tracking.
    
    The previous components remain unchanged, ensuring backward compatibility.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import CounterApp from '../useState/CounterApp';
 import Greeting from '../useState/greeting';
 import Todo from '../useState/Todo';
 import Logger from '../useEffect/Logger';
+import Timer from '../useEffect/Timer';
 
 function App() {
   
@@ -14,8 +15,10 @@ function App() {
       {/* <Todo />
       <CounterApp />
       <Greeting />
-      <h2>Welcome to my app</h2> */}
+      <h2>Welcome to my app</h2> 
       <Logger />
+      */}
+      <Timer />
     </>
   );
 }
