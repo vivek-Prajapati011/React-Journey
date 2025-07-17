fb438a7459b4b907a9345674e455c45fb4f29082
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-17 14:06:52 +0530

    Adds timer functionality with cleanup to Timer component
    Implements a setInterval to log "Tick" every second, enhancing the Timer component's functionality.
    
    .

--- useEffect/Timer.jsx
+++ useEffect/Timer.jsx
@@ -3,10 +3,21 @@ import { useEffect } from "react";
 
 const Timer = () => {
    useEffect( () =>{
-    
+    const intervalId = setInterval(() => {
+      console.log("Tick")
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId)
+      console.log("Timer stopped")
+    };
     },[])
 
-    return 
+    return  (
+    <div>
+      <h2>Timer is running...</h2>
+    </div>
+  )
       
 }
 
