cbc418944b091cd2462a60e543f88b5a8409d477
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-17 13:06:24 +0530

    Adds console log on component mount
    Implements a `useEffect` hook to log a message to the console when the component mounts.
    
    This change helps in debugging by confirming the component's lifecycle behavior.

--- useEffect/Logger.jsx
+++ useEffect/Logger.jsx
@@ -3,6 +3,16 @@ import { useEffect } from "react";
 
 const Logger = () => {
 
+   useEffect( () =>{
+    console.log("mounted")
+},[])
+
+return (
+    <div>
+      <h1>Check the console!</h1>
+    </div>
+  );
+
 }
 
 export default Logger
