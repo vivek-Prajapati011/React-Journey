ba7be4b87a15a8a553a2c1af5172dfa9196f9fe4
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-17 10:05:17 +0530

    Adds task management functionality to Todo component
    Implements the ability to add tasks in the Todo component.
    
    Introduces state management for task input and todo list.
    
    Validates input to ensure non-empty tasks are added.

--- useState/Todo.jsx
+++ useState/Todo.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import { useState } from "react";
 
 const Todo = () => {
+   const [task,setTask] = useState("")
+   const[todo,setTodo] = useState([])
 
+   const addTask = () => {
+      if(task.trim() !== ""){
+        setTodo([...task, todo])
+        setTask("")
+      }
+   }
+
+ return(
+    <div>
+  
+    </div>
+ )
 }
-
 export default Todo;
