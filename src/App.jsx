c192ea6b5667d5194bbbd824643d22220abd0009
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-18 07:05:38 +0530

    Adds Loader component and comments out Timer
    Introduces the Loader component to enhance the app's functionality.
    
    Temporarily comments out the Timer component for future adjustments or testing.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Greeting from '../useState/greeting';
 import Todo from '../useState/Todo';
 import Logger from '../useEffect/Logger';
 import Timer from '../useEffect/Timer';
+import Loader from '../useEffect/Loader';
 
 function App() {
   
@@ -18,7 +19,8 @@ function App() {
       <h2>Welcome to my app</h2> 
       <Logger />
       */}
-      <Timer />
+    {/* <Timer /> */}
+    <Loader />
     </>
   );
 }
