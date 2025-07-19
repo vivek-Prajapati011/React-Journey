152bee582022f15972be230da114bb8908ed1198
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-19 19:42:03 +0530

    Adds MovieDisplay component to show movie name
    Introduces a new MovieDisplay component that renders the name of a movie passed as a prop.
    
    This enhances the user interface by allowing dynamic movie name display in the application.

---
+++ Lift-state-up/MoiveDisplay.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MovieDisplay = ({moviename}) =>{
+    return (
+    <div>
+      <h3>Movie Name: {moviename}</h3>
+    </div>
+  )
+
+}
+
+export default MovieDisplay
