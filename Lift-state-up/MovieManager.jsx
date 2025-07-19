988e2668e4f9da4955846e08c3d6b9892d3103b7
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-19 19:41:26 +0530

    Adds movie name input and display functionality
    Implements a new input component for movie names and displays the entered movie name in the Movie Manager. This enhancement allows users to manage movie titles effectively, improving the overall user experience.

--- Lift-state-up/MovieManager.jsx
+++ Lift-state-up/MovieManager.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { useState } from "react";
+import Input from "./Input";
+
 
 const MovieManager = () => {
+   const [moviename, setMovieName] = useState("")
 
+   const handleMovieChange = (newName) => {
+       setMovieName(newName )
+   }
+
+   return (
+    <div>
+      <h2> Movie Manager</h2>
+      <MovieInput onMovieChange={handleMovieChange} />
+      <MovieDisplay movieName={moviename} />
+    </div>
+  )
 }
 
 export default MovieManager
