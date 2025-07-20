728b20ccf14dee8423b4b2bd40a3e442614f1f77
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-20 15:58:54 +0530

    Adds input focus functionality with useRef hook
    Implements a focus feature for the input element using the
    useRef hook to manage references.
    
    Includes a button that triggers the focus on the input
    when clicked, enhancing user interaction.

--- useEffect/UseRefFocusInput.jsx
+++ useEffect/UseRefFocusInput.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import { useRef } from "react";
 
 const UseRefFocusInput = () => {
-
+   const inputRef = useRef(null)
+   const handleFocus = () => {
+    inputRef.current.focus()
+   }
 }
+
+return (
+    <div>
+        <input ref={inputRef} type="text" />
+        <button onClick={handleFocus}>focus input</button>
+    </div>
+)
 
 export default UseRefFocusInput
