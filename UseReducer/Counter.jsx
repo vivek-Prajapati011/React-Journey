cb7015744b4fd3d2c6da0ff5c53bf089547ffbd3
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-21 19:37:44 +0530

    Adds counter functionality with reducer logic
    Implements a state management system for a counter component using the useReducer hook.
    
    Introduces actions for incrementing, decrementing, and resetting the counter, enhancing user interaction and functionality.

--- UseReducer/Counter.jsx
+++ UseReducer/Counter.jsx
@@ -2,6 +2,21 @@ import React from "react";
 import { useReducer } from "react";
 
 const Counter = () => {
+    const intialState = {count : 0}
+    const reducer = (state, action) => {
+        switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1 };
+    case "DECREMENT":
+      return { count: state.count - 1 };
+    case "RESET":
+      return { count: 0 };
+    default:
+      return state;
+  }
+
+
+    }
 
 }
 
