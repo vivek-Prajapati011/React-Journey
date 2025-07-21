cbad79a94e4686e9be3bdb3519e428856b0b5d22
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-21 21:04:18 +0530

    write inline css for card

--- Component compostion/Card.jsx
+++ Component compostion/Card.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Card = () => {
-
+const Card = ({children}) => {
+     const style = {
+    width: "250px",
+    border: "1px solid #ddd",
+    borderRadius: "12px",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+    padding: "16px",
+    fontFamily: "Arial, sans-serif",
+    backgroundColor: "#fff",
+    textAlign: "center",
+  }
+   return (
+    <div style={style}>{children} </div>
+   )
 }
 
 export default Card
