d91f2b40358b6296f15b1a1842a948ef5972ffff
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-22 08:01:12 +0530

    fix some bug and using useState for Adding Dynamic Product List

--- shopping-card/Product.jsx
+++ shopping-card/Product.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../Component compostion/Card";
 
 const Product = () => {
+  const [product, setProduct] = useState([
+    { id: 1, name: "Headphones", price: 1999, img: "https://via.placeholder.com/150" },
+    { id: 2, name: "Keyboard", price: 899, img: "https://via.placeholder.com/150" },
+    { id: 3, name: "Mouse", price: 599, img: "https://via.placeholder.com/150" }
+  ]);
 
-}
+  return (
+    <div>
+      {product.map((item) => (
+        <li key={item.id}>
+          <Card name={item.name} price={item.price} img={item.img} />
+        </li>
+      ))}
+    </div>
+  );
+};
 
-export default Product
+export default Product;
