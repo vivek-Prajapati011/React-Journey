2308dfab39f4aecb3bc898135b6afac653aa7110
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-23 14:43:35 +0530

    Create basic form with email input and submit button

--- form/BasicForm.jsx
+++ form/BasicForm.jsx
@@ -3,6 +3,17 @@ import { useState } from "react";
 
 const BasicForm = () => {
 
+return (
+    <>
+    <form>
+        <label>
+            entre your gmail
+            <input type="email"/>
+        </label>
+        <button>submit</button>
+    </form>
+    </>
+)
 }
 
 export default BasicForm
