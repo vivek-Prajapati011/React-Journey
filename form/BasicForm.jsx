e77198c3ba67d79673e2adf4f155a495b5185b7d
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-23 15:00:57 +0530

    write logic in handlesubit to prevent from reloading

--- form/BasicForm.jsx
+++ form/BasicForm.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import { useState } from "react";
 
 const BasicForm = () => {
+    const[email, setEmail] = useState()
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+    }
 
 return (
     <>
     <form>
         <label>
             entre your gmail
-            <input type="email"/>
+            <input 
+               type="email"
+
+            />
         </label>
-        <button>submit</button>
+        <button onClick={handleSubmit}>submit</button>
     </form>
     </>
 )
