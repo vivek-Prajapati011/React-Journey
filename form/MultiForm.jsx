c63b343a157bcfdabe25a5116368961f1e8dc60a
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-23 15:59:09 +0530

    set name + email using a single state object

--- form/MultiForm.jsx
+++ form/MultiForm.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { useState } from "react";
 
 const MultiForm = () => {
-
+    const[formData, setFormData] = useState({name:"", email:""})
+    
+    
 }
 
 export default MultiForm
