18dcc83446b35a6bc037514f05291f38853cf30d
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-23 16:04:13 +0530

    set input for email and name and submit button

--- form/MultiForm.jsx
+++ form/MultiForm.jsx
@@ -3,6 +3,18 @@ import { useState } from "react";
 
 const MultiForm = () => {
     const[formData, setFormData] = useState({name:"", email:""})
+    return (
+        <>
+        <input type="text"
+        value={formData.name}
+        />
+        <input type="email"
+        value={formData.email}
+        />
+        <button>submit</button>
+        </>
+        
+    )
     
     
 }
